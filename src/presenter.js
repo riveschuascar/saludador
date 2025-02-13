66e9d04b23c695a790be4fe3c89970e31e628777
unknown <huascar.rivero@ucb.edu.bo>
2025-02-13 06:26:02 -0800

    feat: saludar por hora

--- src/presenter.js
+++ src/presenter.js
@@ -4,10 +4,21 @@ const input = document.querySelector("#name");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
-  
+  let fechaActual = new Date();
+  let horaActual = fechaActual.getHours();
   const nombre = input.value.trim();
+  let saludoHora;
+  if (horaActual <= 12) {
+    saludoHora = "Buenos dias";
+  }
+  else if (horaActual <= 18) {
+    saludoHora = "Buenas tardes";
+  }
+  else {
+    saludoHora = "Buenas noches";
+  }
 
-  let saludar_nombre = "Hola " + nombre + "!";
-  div.textContent = saludar_nombre;
+  let saludar = saludoHora + " " + nombre + "!";
+  div.textContent = saludar;
   console.log(saludar_nombre);
 });
