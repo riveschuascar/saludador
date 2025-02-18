d5ee63e92db4aa91a3e3c834dccb988fe3b0077f
unknown <huascar.rivero@ucb.edu.bo>
2025-02-18 03:48:15 -0800

    feat: saludar por genero

--- src/presenter.js
+++ src/presenter.js
@@ -1,12 +1,11 @@
 const form = document.querySelector("#pedir-nombre");
 const div = document.querySelector("#saludo");
+const select = document.querySelector("#genero")
 const input = document.querySelector("#name");
 
-form.addEventListener("submit", (event) => {
-  event.preventDefault();
+function getCurrTime() {
   let fechaActual = new Date();
   let horaActual = fechaActual.getHours();
-  const nombre = input.value.trim();
   let saludoHora;
   if (horaActual <= 12) {
     saludoHora = "Buenos dias";
@@ -17,8 +16,27 @@ form.addEventListener("submit", (event) => {
   else {
     saludoHora = "Buenas noches";
   }
+  return saludoHora;
+}
 
-  let saludar = saludoHora + " " + nombre + "!";
+function getGender(gender) {
+  if (gender === "Masculino") {
+    return "Sr.";
+  }
+  else if (gender === "Femenino") {
+    return "Sra.";
+  }
+  else {
+    return "";
+  }
+}
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const nombre = input.value.trim();
+  hora = getCurrTime();
+  genero = getGender(select.value)
+  let saludar = hora + " " + genero + " " + nombre + "!";
   div.textContent = saludar;
   console.log(saludar_nombre);
 });
